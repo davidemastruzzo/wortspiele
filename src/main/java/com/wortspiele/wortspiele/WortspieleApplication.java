52afb6f8f70356658b6914960547205584bdcf96
Davide Mastruzzo <davide.mastruzzo@yes.swiss>
2023-08-07 17:41:57 +0200

    first commit

---
+++ src/main/java/com/wortspiele/wortspiele/WortspieleApplication.java
@@ -0,0 +1,13 @@
+package com.wortspiele.wortspiele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WortspieleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WortspieleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wortspiele/wortspiele/WortspieleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wortspiele.wortspiele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WortspieleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
